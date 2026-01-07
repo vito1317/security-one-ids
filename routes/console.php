ec3b0b38e11d631b6843b97931747121685f29f0
vito1317 <service@vito1317.com>
2026-01-07 08:36:27 +0000

    feat: schedule WAF heartbeat every minute

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,15 @@
 <?php
 
+use App\Services\WafSyncService;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+// Send heartbeat to WAF every minute
+Schedule::call(function (WafSyncService $wafSync) {
+    $wafSync->heartbeat();
+})->everyMinute();
