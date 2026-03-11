733c994bf04fb943f2cc20b9dc4eb206ce873e3e
柯瑋宸 <97001360+vito1317@users.noreply.github.com>
2026-03-12 01:42:41 +0800

    refactor: set DetectsPlatform::isWindows visibility to private (#75)
    Merge PR #75: refactor: set DetectsPlatform::isWindows visibility to private

--- app/Services/Detection/SuricataEngine.php
+++ app/Services/Detection/SuricataEngine.php
@@ -5,6 +5,7 @@ namespace App\Services\Detection;
 use App\Traits\DetectsPlatform;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Process;
+use App\Traits\DetectsPlatform;
 
 /**
  * Suricata IDS/IPS Detection Engine
