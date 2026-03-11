9787c11b02d718b6dc770b41fc4d6054df1b4b1e
vito1317 <97001360+vito1317@users.noreply.github.com>
2026-03-11 20:52:37 +0000

    Fix duplicated imports after merge

--- app/Http/Controllers/Api/SystemUpdateController.php
+++ app/Http/Controllers/Api/SystemUpdateController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Artisan;
-use Illuminate\Support\Facades\Log;
 
 class SystemUpdateController extends Controller
 {
